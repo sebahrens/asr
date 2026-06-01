c60cd22e324b36c2e9fd148fb5b418bde110e9d7
platon2001 <platon2001@icloud.com>
2026-06-01 12:06:25 +0200

    [asr-xubq] Fix ACR build Dockerfile contexts

--- deploy/azure/scripts/acr-build-services.sh
+++ deploy/azure/scripts/acr-build-services.sh
@@ -14,7 +14,20 @@ fi
 
 GIT_SHA=$(git rev-parse --short HEAD)
 
-az acr build --registry "$ACR" --image "asr-api:${GIT_SHA}" ./deploy/docker/api
-az acr build --registry "$ACR" --image "asr-web:${GIT_SHA}" ./packages/web
+az acr build \
+  --registry "$ACR" \
+  --image "asr-api:${GIT_SHA}" \
+  --file deploy/docker/Dockerfile.api \
+  .
+
+az acr build \
+  --registry "$ACR" \
+  --image "asr-web:${GIT_SHA}" \
+  --file deploy/docker/Dockerfile.web \
+  --build-arg ASR_WEB_BUILD_PROFILE=production \
+  --build-arg VITE_API_URL=/api \
+  --build-arg VITE_AUTH_MODE=msal \
+  --build-arg VITE_ENABLE_MOCK_AUTH=false \
+  .
 
 echo "Built ${ACR}.azurecr.io/asr-api:${GIT_SHA} and ${ACR}.azurecr.io/asr-web:${GIT_SHA}"
